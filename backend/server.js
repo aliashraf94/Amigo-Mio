348be451c4d9fcc7b4ca15d0b39ec34fefa8b11f
Ali Ashraf <adaydreamer10@gmail.com>
2021-07-26 18:04:03 +0200

    sign-in/sign-up with jwt auth

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const cors = require("cors");
+const userRouter =  require('./routes/user')
+
+// initializing express application
 const app = express();
 
+// parse requests of content-type - application/json
+app.use(express.json());
 
+const corsOptions = {
+    origin: "http://localhost:3000"
+  };
+
+app.use(cors(corsOptions));  // enable CORS
+
+// Simple route
 app.get('/', (req, res) => {
     res.send('Welcome to the final project :-)')
 });
 
-app.listen(4000, () => console.log("Server is up and running"))
+app.use("/user", userRouter);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
