4574c3b4539b46fa4358877854bc60c154d714bb
Ali Ashraf <adaydreamer10@gmail.com>
2021-08-04 15:22:23 +0200

    removing a links from paragraphs

--- frontend/src/components/Donate.jsx
+++ frontend/src/components/Donate.jsx
@@ -11,7 +11,6 @@ const Donate = () => {
                 <div className="media-body twoColumn">
                     <h5 className="mt-0 mb-1">DONATE AND RECEIVE OUR BOOK</h5>
                     Every person who donates more than 10 euros to our organization through this page will be able to receive the book. The funds raised through these donations will be used to continue the activities of the Open Cultural Center in Greece.
-                    <a href="https://openculturalcenter.org/es/producto/my-friend/"   target="_blank" rel="noreferrer"> You can buy the book here.</a>
                 </div>
                 <img className="ml-3 d-none d-sm-block" width="10%" src={donate_amic_meu} alt="Generic placeholder image" />
          </div>
