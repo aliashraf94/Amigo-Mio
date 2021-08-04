03ca06e66b3816d7856ba239b82ab596100bfe1f
Ali Ashraf <adaydreamer10@gmail.com>
2021-08-04 14:49:29 +0200

    section mainbar

--- frontend/src/components/SectionMainBanner.jsx
+++ frontend/src/components/SectionMainBanner.jsx
@@ -11,17 +11,6 @@ const SectionMainBanner = () => {
       <header>
         <div className="container-fluid">
           <div className="contenido">
-<<<<<<< HEAD
-            <img className="element-animation tree-img img-fluid float-left"
-                src={tree}> 
-            </img>
-            <img className="element-animation2 art-person-img img-fluid  float-left"
-                src={art_person} > 
-            </img>
-            <img className="element-animation3 amigomio-img img-fluid  float-left"
-                src={amigo_mio} alt="amigo mio" > 
-            </img>
-=======
             <div className="backgroundMain" >
             <div className="cuadroparatextos">
                   <h1>Welcome to Amigo Mío</h1>
@@ -29,7 +18,6 @@ const SectionMainBanner = () => {
                   <a className="boton" href="#section">Lear More</a>
             </div>
             </div>
->>>>>>> 96337563a2e778815bf2078ad0edd9d1685d8831
           </div>
         </div>
       </header>
