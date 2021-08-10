666bfdd3ac85987d8bdc80773eb4d9d791ddc36e
adriancorro <adriancorrog@gmail.com>
2021-08-10 13:38:36 +0200

    The slider component has been improved and console errors have been eliminated

--- frontend/src/components/Slides.jsx
+++ frontend/src/components/Slides.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import React, { useState, useEffect } from "react";
+import SimpleImageSlider from "react-simple-image-slider";
 import slides_1 from '../assets/images/slides_1.JPG';
 import slides_2 from '../assets/images/slides_2.png';
 import slides_3 from '../assets/images/slides_3.png';
@@ -9,18 +8,49 @@ import slides_5 from '../assets/images/slides_5.png';
 import slides_6 from '../assets/images/slides_6.png';
 
 
-const Slides = () => {
+const images = [
+    { url: slides_1 },
+    { url: slides_3 },
+    { url: slides_4 },
+    { url: slides_5 },
+    { url: slides_6 },
+    { url: slides_2 }
+  ];
+
+  
+
+  
+  
+   
+  
+  
+  const Slides = () => {
+
+
+    const [isDesktop, setDesktop] = useState(window.innerWidth > 1450);
+
+    const updateMedia = () => {
+      setDesktop(window.innerWidth > 600);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("resize", updateMedia);
+      return () => window.removeEventListener("resize", updateMedia);
+    });
 
     return (
-                    <AwesomeSlider>
-                        <div data-src={slides_1} />
-                        <div data-src={slides_2} />
-                        <div data-src={slides_3} />
-                        <div data-src={slides_4} />
-                        <div data-src={slides_5} />
-                        <div data-src={slides_6} />
-                    </AwesomeSlider>
+      <div>
+        <SimpleImageSlider
+          width={isDesktop ? '520px' : '320px'}
+          height={isDesktop ? '400px' : '290px'}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+          slideDuration = {0.5}
+          startIndex = {2}
+        />
+      </div>
     );
-};
+  }
 
 export default Slides;
