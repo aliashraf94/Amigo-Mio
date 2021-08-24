58c614c5488beae3e8cab4edde262ef116681ac6
adriancorro <adriancorrog@gmail.com>
2021-08-24 15:05:19 +0200

    only logged in users can see the favorites button. correct

--- frontend/src/components/CarouselItem.jsx
+++ frontend/src/components/CarouselItem.jsx
@@ -110,7 +110,9 @@ let CarouselItem = (props)=> {
           <div className="carousel-item__details">
             <p className="carousel-item__details--title">{result.title}  </p>
             <p className="carousel-item__details--subtitle">Likes: {result.likes} </p>
-            {dataUser.length && dataUser.is_admin &&
+            {console.log(dataUser)}
+            {dataUser.length && dataUser[0].is_admin &&
+            
             <div className="img_container"  onClick={getIdBook}  data-value={result.id } >
                 <img src={img} className="save_img"/> 
             </div>}
